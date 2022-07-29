fd59db3a95bca3ee9eb778ccfee46f925a29090e
Jaewon Kim <100887534+bloodsilk-jwk@users.noreply.github.com>
2022-07-30 06:47:16 +0900

    First

---
+++ CSharpfirst/CSharpfirst/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpfirst
+{
+    class Program
+    {
+        static void Main(string[] args) // 지정하지 않으면 실행 안 됨
+        {
+            Console.Write("Hello World!"); // 줄바꿈 없음
+            Console.WriteLine("이것저것"); // 줄바꿈 있음
+            Console.Beep(); // 소리내기
+        }
+    }
+}
